17448b4669d6ec9ae8df6f4b9273556ee8396d92
Cox Chen <coxchen@gmail.com>
2018-05-31 17:19:04 +0800

    hide entry logs from backlog

--- src/cljs/punch/views_reporting.cljs
+++ src/cljs/punch/views_reporting.cljs
@@ -9,55 +9,54 @@
 (defn ->logs [logs]
   (flatten (for [d logs] (for [a d] (if (string? a) a (str (first a) " " (gstring/unescapeEntities "&rarr;") " " (last a)))))))
 
-(defn reporting-entries [group-by-proj proj]
-  (into [:ul
-         (for [[idx entry] (map-indexed (fn [idx itm] [idx itm]) (get group-by-proj proj))]
-           ^{:key idx}
-           [:li
-            (if (not-empty (:jira entry)) (str (:jira entry) ": "))
-            (:topic entry)
-            (let [logs (vals (:logs entry))
-                  _ (.log js/console "??" (pr-str logs))]
-              (into [:ul]
-                    (for [l (->logs logs)]
-                      [:li l])))
-          ])]))
+(defn reporting-logs [entry]
+  (fn []
+    (let [logs (vals (:logs entry))
+          _ (.log js/console "??" (pr-str logs))]
+      (into [:ul]
+            (for [l (->logs logs)]
+              [:li l])))))
 
-(defn reporting-projects [group-by-ver ver]
+(defn reporting-entries [group-by-proj proj include-log?]
+  (fn []
+    (into [:ul]
+          (for [[idx entry] (map-indexed (fn [idx itm] [idx itm]) (get group-by-proj proj))]
+            ^{:key idx}
+            [:li
+             (if (not-empty (:jira entry)) (str (:jira entry) ": "))
+             (:topic entry)
+             (if include-log? [reporting-logs entry])]))))
+
+(defn reporting-projects [group-by-ver ver include-log?]
   (let [group-by-proj (group-by :project (get group-by-ver ver))
         _ (.log js/console "group-by-proj" (pr-str group-by-proj))]
     (into [:div [:h3 (or ver "UNKNOWN_VERSION")]]
           (for [[idx p] (map-indexed (fn [idx itm] [idx itm]) (keys group-by-proj))]
             ^{:key idx} [:div
                          (if (and (not= p "None") (not-empty p)) [:h4 "[project] " p])
-                         [reporting-entries group-by-proj p]]))))
+                         [reporting-entries group-by-proj p include-log?]]))))
 
-(defn reporting-items [items versions separatorText]
+(defn reporting-items [items versions separatorText include-log?]
   (let [_ (.log js/console (pr-str (group-by :version @items)))
         group-by-ver (group-by :version @items)]
-    (into [:div
-            [:h3 "--------------------" separatorText "--------------------"]]
+    (into [:div [:h3 "-------------------- " separatorText " --------------------"]]
           (for [[idx ver] (map-indexed (fn [idx itm] [idx itm]) (keys group-by-ver))]
-            ^{:key idx} [reporting-projects group-by-ver ver]))))
+            ^{:key idx} [reporting-projects group-by-ver ver include-log?]))))
 
 (defn reporting-problem []
   [:div
-    [:h3 "--------------------Problem--------------------"]
+    [:h3 "-------------------- Problem --------------------"]
     [:ul [:li [:topic "None"]]]])
 
 (defn reporting []
   (let [entries  (re-frame/subscribe [:entries])
         projects (re-frame/subscribe [:projects])
         versions (re-frame/subscribe [:versions])
-        backlog (re-frame/subscribe  [:backlog])]
+        backlog  (re-frame/subscribe [:backlog])]
     [:div
-      [reporting-items entries versions "Progress"]
-      [reporting-items backlog versions "Plan"]
-      [reporting-problem]
-    ]
-  )
-)
-
+      [reporting-items entries versions "Progress" true]
+      [reporting-items backlog versions "Plan" false]
+      [reporting-problem]]))
 
 (defn reporting-modal []
   [:> modal {:trigger (r/as-element [:> button {:icon "list" :class "violet circular mini" :content "report"}])
